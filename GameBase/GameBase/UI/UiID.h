b3f67b5e9b04e0a8e6e13d50971531e48a60d72d
Hikarunn <hikaru.0312.0708@gmail.com>
2023-05-11 15:03:28 +0900

    各種UIの追加

--- GameBase/GameBase/UI/UiID.h
+++ GameBase/GameBase/UI/UiID.h
@@ -3,12 +3,13 @@
 // UIの種類
 enum class UiID
 {
-	Torion,
-	Skill,
-	Icon,
-	Button,
-	Combo,
-	Time,
-	Mess,
-	Max
+	Torion,			// プレイヤーのエネルギー 
+	Icon,			// アイコン各種
+	Button,			// ボタン
+	Frame,			// 枠
+	Combo,			// コンボ数
+	Qursor,			// カーソル
+	Time,			// 時間
+	Mess,			// メッセージ
+	Max					
 };
