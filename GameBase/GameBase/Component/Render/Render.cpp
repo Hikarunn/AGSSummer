a9f3176211f6d60ffecc1612b343bb563c435b5b
Hikarunn <hikaru.0312.0708@gmail.com>
2023-05-11 10:09:21 +0900

    コライダーの更新

--- GameBase/GameBase/Component/Render/Render.cpp
+++ GameBase/GameBase/Component/Render/Render.cpp
@@ -1,1 +1,13 @@
+#include "../../Object/ObjectManager.h"
 #include "Render.h"
+
+Render::~Render()
+{
+}
+
+void Render::Begin(ObjectManager& objectManager)
+{
+	transform_ = objectManager.GetComponent<Transform>(ownerId_);
+}
+
+
