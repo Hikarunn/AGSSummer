8ca63d4f436b3abf27a051e13987eb725ecb1938
Hikarunn <hikaru.0312.0708@gmail.com>
2023-05-11 15:01:15 +0900

    ObjectAttributeの更新

--- GameBase/GameBase/Component/Info/ObjectAttribute.h
+++ GameBase/GameBase/Component/Info/ObjectAttribute.h
@@ -4,7 +4,10 @@
 enum class ObjectAttribute
 {
 	Player,				// プレイヤー
+	PlayerAttack,		// プレイヤーの攻撃範囲
+	PlayerShot,			// プレイヤーのショット
 	Enemy,				// 敵
+	EnemyAttack,		// 敵の攻撃範囲
 	Stage,				// ステージ
 	Sky,				// 空
 	Other				// それ以外
