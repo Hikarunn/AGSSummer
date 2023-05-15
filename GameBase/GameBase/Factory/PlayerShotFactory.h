03a3196e9bc6d80e16b7c1ea7eaebe42a8a85ef8
Hikarunn <hikaru.0312.0708@gmail.com>
2023-05-15 15:10:34 +0900

    プレイヤーの射撃クラス(今後微調整必須)

--- GameBase/GameBase/Factory/PlayerShotFactory.h
+++ GameBase/GameBase/Factory/PlayerShotFactory.h
@@ -8,6 +8,6 @@ public:
 private:
 	ObjectID Create(ObjectID ownerID, const Vector3& pos, const Vector3& rot = { 0.0f,0.0f,0.0f }) final;
 	void Destroy(ObjectID id) final;
-	const FactoryID GetID(void) const { return FactoryID::PlayerSkill; }
+	const FactoryID GetID(void) const { return FactoryID::PlayerShot; }
 };
 
