ff1e161a177265d1c5d29ccfffea763ae7278a56
Hikarunn <hikaru.0312.0708@gmail.com>
2023-07-12 13:33:04 +0900

    シェーダーコード作成

---
+++ GameBase/GameBase/Shader/ShadowMap/ShadowMapPixelShader.hlsl
@@ -0,0 +1,23 @@
+struct PixelShaderInput {
+	float4 pos:POSITION0;
+	float4 vpos:POSITION1;
+	float3 norm:NORMAL;
+	float4 uv:TEXCOORD;
+	float4 diff:COLOR0;
+	float4 spec:COLOR1;
+	float4 svpos:SV_POSITION;
+};
+
+struct PSOutput {
+	float4 color:SV_TARGET0;
+};
+
+PSOutput main(PSInput input)
+{
+	PSOutput output;
+	// ZílÇêFÇ∆ÇµÇƒèoóÕ
+	output.color = input.vpos.z / input.vpos.w;
+	// ìßñæÇ…Ç»ÇÁÇ»Ç¢ÇÊÇ§Ç…
+	output.color.a = 1.0f;
+	return output;
+}
